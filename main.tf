d3ff3d1debd7dc06aa80f070da1086a967187f91
= <=>
2024-01-05 13:16:10 +0530

    updated main

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 ###########################################
 
 resource "random_string" "myrand_string" {
-    count = 2
+  count   = 2
   length  = 5
   upper   = false
   special = false
@@ -15,7 +15,7 @@ resource "random_string" "myrand_string" {
 
 resource "google_compute_instance" "this" {
   count        = 2
-  name         = format("%s-%s",var.instance_name, random_string.myrand_string[count.index].id)
+  name         = format("%s-%s", var.instance_name, random_string.myrand_string[count.index].id)
   machine_type = "e2-small"
   zone         = var.zone
 
@@ -28,7 +28,7 @@ resource "google_compute_instance" "this" {
       image = "debian-cloud/debian-11"
     }
   }
-    
+
 }
 
 ################################################
@@ -36,7 +36,7 @@ resource "google_compute_instance" "this" {
 ################################################
 
 resource "google_monitoring_dashboard" "dashboard" {
-    project = var.project_id
+  project        = var.project_id
   dashboard_json = file("C:/Users/myrem/Desktop/terraform/Git/GCP_Terraform/GCP_Terraform/dashboard.json")
 }
 ############################################################
@@ -45,25 +45,25 @@ resource "google_monitoring_dashboard" "dashboard" {
 
 resource "google_monitoring_alert_policy" "alert_policy" {
   display_name = "CPU Utilization > 30%"
-  
-  combiner     = "OR"
+
+  combiner = "OR"
   conditions {
     display_name = "Condition 1"
     condition_threshold {
-        comparison = "COMPARISON_GT"
-        duration = "60s"
-        filter = "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\""
-        threshold_value = "0.3"
-        trigger {
-          count = "1"
-        }
+      comparison      = "COMPARISON_GT"
+      duration        = "60s"
+      filter          = "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\""
+      threshold_value = "0.3"
+      trigger {
+        count = "1"
+      }
     }
   }
 
   alert_strategy {
     notification_channel_strategy {
-        renotify_interval = "1800s"
-        notification_channel_names = [google_monitoring_notification_channel.email.name]
+      renotify_interval          = "1800s"
+      notification_channel_names = [google_monitoring_notification_channel.email.name]
     }
   }
 
@@ -79,9 +79,9 @@ resource "google_monitoring_alert_policy" "alert_policy" {
 #############################################
 
 resource "google_monitoring_notification_channel" "email" {
- display_name = "Tier 1 Support Email"
-   type = "email"
-   labels = {
-     email_address = var.email
-   }
- }
+  display_name = "Tier 1 Support Email"
+  type         = "email"
+  labels = {
+    email_address = var.email
+  }
+}
