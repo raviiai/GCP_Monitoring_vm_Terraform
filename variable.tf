5343d569b7cf72b9cfa3b9ebfd8eb85971dbde10
= <=>
2024-01-05 13:25:18 +0530

    updated variable.tfvars

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 variable "instance_name" {
-  default = "my-instance"
+
 }
 
 variable "zone" {
@@ -7,9 +7,8 @@ variable "zone" {
 }
 
 variable "project_id" {
-  default = "warm-bonfire-337209"
 }
 
 variable "email" {
-  default = "raviiprakash.ai@gmail.com"
+ 
 }
